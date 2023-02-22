b3de1b362641060d2ada5ea0a161fbbf2ab43424
Cole Waggoner <waggcj02@gmail.com>
2023-02-22 16:32:34 -0500

    Quesions now penalize for wrong answers and when correct the next question is loaded

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -5,6 +5,7 @@ var time = 60;
 function quizStart(event) {
   event.stopPropagation();
   document.getElementById("start").className = "hidden";
+  document.getElementById("help").className = "hidden";
   document.getElementById("quiz").className = "show";
   getQuiz();
   // timerStart();
@@ -17,12 +18,41 @@ function getQuiz() {
   for (var i = 0; i < quizValue.options.length; i++) {
     var button = document.createElement("button");
     button.textContent = quizValue.options[i];
-    button.id="options";
-    button.setAttribute("ans-button", i);
+    button.id = "options";
+    button.setAttribute("ans", i);
     document.querySelector("#answers").appendChild(button);
   }
 }
 
-
+function answerAttempt(event) {
+  var element = event.target;
+  if (element.matches("button") !== true) {
+  } else {
+    var quizValue = quiz[questionNum];
+    if (quizValue.answer !== quizValue.options[element.getAttribute("ans")]) {
+      if (time > 5) {
+        time -= 5;
+        document.getElementById("secs").textContent = time;
+      } else {
+        time -= time;
+        document.getElementById("secs").textContent = time;
+        quizEnd();
+      }
+      document.getElementById("help").textContent = "Try Again!";
+      document.getElementById("help").className = "show";
+    } else {
+      console.log("Correct Answer");
+      document.getElementById("help").textContent = "Right!";
+      document.getElementById("help").className = "show";
+      questionNum++;
+      if (questionNum > 3) {
+        quizEnd();
+      } else {
+        getQuiz();
+      }
+    }
+  }
+}
 
 document.querySelector("#start").addEventListener("click", quizStart);
+document.querySelector("#answers").addEventListener("click", answerAttempt);
